3ac57aa2ce4a92a531a2e9c68dd8d1ce397d143b
Raúl Arcos Herrera <1.raularcos@gmail.com>
2023-09-15 19:49:46 +0200

    eleccion de animación e index inicial

--- js/animations.js
+++ js/animations.js
@@ -1,45 +1,234 @@
-var nameArray = ["cow", "horse", "slug", "pinata", "crow"]; //One of those will be randomly chosen
-
-var lastIndex = 0; //Used to remember the last word picked
-
-var domHeaderName = document.getElementsByClassName("nameContainer")[0]; //Put the ID of the DOM element you want to change here
-
-var bWhichContainer = 0;
-
-//This function will get a random INT used later as the array index
-function getRandomInt(iMin, iMax) {
-    var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
-    while( lastIndex == newIndex ){
-        var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
-    }
-  lastIndex = newIndex;  
-
-  return newIndex;
-}
-
-function switchContainer( domContainer, index ) {
+function WordShuffler(holder,opt){
+    var that = this;
+    var time = 0;
+    this.now;
+    this.then = Date.now();
     
+    this.delta;
+    this.currentTimeOffset = 0;
     
-    document.getElementsByClassName("nameContainer")[bWhichContainer].classList.add("hidden");
+    this.word = null;
+    this.currentWord = null;
+    this.currentCharacter = 0;
+    this.currentWordLength = 0;
+  
+  
+    var options = {
+      fps : 120,
+      timeOffset : 5,
+      textColor : '#000',
+      fontSize : "50px",
+      useCanvas : false,
+      mixCapital : false,
+      mixSpecialCharacters : false,
+      needUpdate : true,
     
-    if( bWhichContainer == 0){
-        bWhichContainer = 1;
+        colors: [
+            '#87CEEB', // Azul claro
+            '#ADD8E6', // Azul cielo
+            '#B0E0E6', // Celeste
+            '#00BFFF', // Azul real
+            '#1E90FF', // Azul claro intenso
+            '#6495ED', // Azul acero
+            '#4682B4', // Azul apagado
+            '#5F9EA0', // Azul cadete
+            '#00CED1', // Azul oscuro intenso
+            '#20B2AA', // Azul claro medio
+            '#AFEEEE', // Turquesa pastel
+            '#87CEFA', // Azul pálido
+            '#00FFFF', // Cian
+            '#E0FFFF', // Azul claro muy pálido
+            '#F0FFFF', // Azul cielo pastel
+            '#B0C4DE', // Azul acero claro
+            '#00CED1', // Azul oscuro intenso
+            '#20B2AA', // Azul claro medio
+            '#AFEEEE', // Turquesa pastel
+            '#87CEFA', // Azul pálido
+            '#00FFFF', // Cian
+            '#E0FFFF', // Azul claro muy pálido
+            '#F0FFFF', // Azul cielo pastel
+            '#B0C4DE', // Azul acero claro
+            '#5F9EA0', // Azul cadete
+            '#4682B4', // Azul apagado
+            '#6495ED', // Azul acero
+            '#1E90FF', // Azul claro intenso
+            '#00BFFF', // Azul real
+        ]
     }
-    else {
-        bWhichContainer = 0;
+  
+    if(typeof opt != "undefined"){
+      for(key in opt){
+        options[key] = opt[key];
+      }
     }
-  //Change the class name here to fit yours
-    document.getElementsByClassName("nameContainer")[bWhichContainer].classList.remove("hidden");
-    document.getElementsByClassName("nameContainer")[bWhichContainer].innerHTML = nameArray[index];
-}
-
-function pickRandomName() {
-    var randomIndex = getRandomInt(0, nameArray.length);
-
-    //Put the randomly chosen name in the DOM
-    switchContainer(domHeaderName, randomIndex);
-    //loop
-    setTimeout(pickRandomName, 2000); //Loop every 2000ms (2s)
-}
-
-pickRandomName();
+  
+  
+    
+    this.needUpdate = true;
+    this.fps = options.fps;
+    this.interval = 1000/this.fps;
+    this.timeOffset = options.timeOffset;
+    this.textColor = options.textColor;
+    this.fontSize = options.fontSize;
+    this.mixCapital = options.mixCapital;
+    this.mixSpecialCharacters = options.mixSpecialCharacters;
+    this.colors = options.colors;
+  
+     this.useCanvas = options.useCanvas;
+    
+    this.chars = [
+      'A','B','C','D',
+      'E','F','G','H',
+      'I','J','K','L',
+      'M','N','O','P',
+      'Q','R','S','T',
+      'U','V','W','X',
+      'Y','Z'
+    ];
+    this.specialCharacters = [
+      '!','§','$','%',
+      '&','/','(',')',
+      '=','?','_','<',
+      '>','^','°','*',
+      '#','-',':',';','~'
+    ]
+  
+    if(this.mixSpecialCharacters){
+      this.chars = this.chars.concat(this.specialCharacters);
+    }
+  
+    this.getRandomColor = function () {
+      var randNum = Math.floor( Math.random() * this.colors.length );
+      return this.colors[randNum];
+    }
+  
+    //if Canvas
+   
+    this.position = {
+      x : 0,
+      y : 50
+    }
+  
+    //if DOM
+    if(typeof holder != "undefined"){
+      this.holder = holder;
+    }
+  
+    if(!this.useCanvas && typeof this.holder == "undefined"){
+      console.warn('Holder must be defined in DOM Mode. Use Canvas or define Holder');
+    }
+  
+  
+    this.getRandCharacter = function(characterToReplace){    
+      if(characterToReplace == " "){
+        return ' ';
+      }
+      var randNum = Math.floor(Math.random() * this.chars.length);
+      var lowChoice =  -.5 + Math.random();
+      var picketCharacter = this.chars[randNum];
+      var choosen = picketCharacter.toLowerCase();
+      if(this.mixCapital){
+        choosen = lowChoice < 0 ? picketCharacter.toLowerCase() : picketCharacter;
+      }
+      return choosen;
+      
+    }
+  
+    this.writeWord = function(word){
+      this.word = word;
+      this.currentWord = word.split('');
+      this.currentWordLength = this.currentWord.length;
+  
+    }
+  
+    this.generateSingleCharacter = function (color,character) {
+      var span = document.createElement('span');
+      span.style.color = color;
+      span.innerHTML = character;
+      return span;
+    }
+  
+    this.updateCharacter = function (time) {
+      
+        this.now = Date.now();
+        this.delta = this.now - this.then;
+  
+         
+  
+        if (this.delta > this.interval) {
+          this.currentTimeOffset++;
+        
+          var word = [];
+  
+          if(this.currentTimeOffset === this.timeOffset && this.currentCharacter !== this.currentWordLength){
+            this.currentCharacter++;
+            this.currentTimeOffset = 0;
+          }
+          for(var k=0;k<this.currentCharacter;k++){
+            word.push(this.currentWord[k]);
+          }
+  
+          for(var i=0;i<this.currentWordLength - this.currentCharacter;i++){
+            word.push(this.getRandCharacter(this.currentWord[this.currentCharacter+i]));
+          }
+  
+  
+          if(that.useCanvas){
+            c.clearRect(0,0,stage.x * stage.dpr , stage.y * stage.dpr);
+            c.font = that.fontSize + " sans-serif";
+            var spacing = 0;
+            word.forEach(function (w,index) {
+              if(index > that.currentCharacter){
+                c.fillStyle = that.getRandomColor();
+              }else{
+                c.fillStyle = that.textColor;
+              }
+              c.fillText(w, that.position.x + spacing, that.position.y);
+              spacing += c.measureText(w).width;
+            });
+          }else{
+  
+            if(that.currentCharacter === that.currentWordLength){
+              that.needUpdate = false;
+            }
+            this.holder.innerHTML = '';
+            word.forEach(function (w,index) {
+              var color = null
+              if(index > that.currentCharacter){
+                color = that.getRandomColor();
+              }else{
+                color = that.textColor;
+              }
+              that.holder.appendChild(that.generateSingleCharacter(color, w));
+            }); 
+          }
+          this.then = this.now - (this.delta % this.interval);
+        }
+    }
+  
+    this.restart = function () {
+      this.currentCharacter = 0;
+      this.needUpdate = true;
+    }
+  
+    function update(time) {
+      time++;
+      if(that.needUpdate){
+        that.updateCharacter(time);
+      }
+      requestAnimationFrame(update);
+    }
+  
+    this.writeWord(this.holder.innerHTML);
+  
+  
+    console.log(this.currentWord);
+    update(time);
+  }
+  
+  var text = document.getElementById('text');
+  
+  var pText = new WordShuffler(text,{
+    textColor : '#000',
+    timeOffset : 2
+  });
